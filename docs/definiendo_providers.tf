8b219467c7e2dbdc2187d6fe7e99f12917c7ffb6
Daniel Ayuga <danielayugachacon@outlook.com>
2025-05-28 21:52:14 +0200

    Update definiendo_providers.tf

--- docs/definiendo_providers.tf
+++ docs/definiendo_providers.tf
@@ -1,6 +1,6 @@
 ## Definiendo providers.tf
 
-En este primer paso, definimos que proveedores vamos a necesitar.
+En este archivo definimos que proveedores vamos a necesitar.
 Los providers contienen la configuración, recursos y datos necesarios para comunicarse con Azure (AWS, GCP..)
 En este ejemplo usaremos dos: 
   -azurerm (Azure Resource Manager) que usaremos para la creación de los grupos de recursos 
@@ -22,10 +22,10 @@ terraform {
 provider "azurerm" {                        #¿Que necesita el provider azurerm para poder crear/modificar/eliminar recursos?
   features {}
 
-  subscription_id = var.subscription_id     #El ID de la suscripción será una variable asociada al archivo secrets.tfvars que se explicará después
+  subscription_id = var.subscription_id     #El ID de la suscripción será una variable asociada al archivo secrets.tfvars
   tenant_id       = var.tenant_id           #El ID del tenant será una variable asociada al archivo secrets.tfvars que se explicará después
 }
 
 provider "azuread" {                        #¿Que necesita el provider azuread para poder crear/modificar/eliminar usuarios?
-  tenant_id     = var.tenant_id             #El ID del tenant será una variable asociada al archivo secrets.tfvars que se explicará después
+  tenant_id     = var.tenant_id             #El ID del tenant será una variable asociada al archivo secrets.tfvars
 }
