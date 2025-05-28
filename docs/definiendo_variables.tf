a24aa11c4f0c921b05d0c3d760b6ff0e1e31dbc6
Daniel Ayuga <danielayugachacon@outlook.com>
2025-05-28 21:58:20 +0200

    Update definiendo_variables.tf

--- docs/definiendo_variables.tf
+++ docs/definiendo_variables.tf
@@ -1,7 +1,7 @@
-variables.tf
+## Definiendo variables.tf
 
 En este ejemplo nuestras variables, solo hacen referencia al nombre que tendrán entre "" y el tipo, que en este caso son string (cadena)
-Reflejamos así que nuestra variable se encuentra en secrets.tfvars (sin tener que usar texto plano) y este nombre lo usaremos en el archivo main.tf de configuración.
+Se asocia esta variable al valor que tienen en secrets.tfvars (sin tener que usar texto plano) y este nombre lo usaremos en el archivo main.tf de configuración.
 
 variable "tenant_id" {
   type = string
