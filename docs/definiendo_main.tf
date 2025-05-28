be0fb9a875166037785ea6f2cf5d84d4d643458d
Daniel Ayuga <danielayugachacon@outlook.com>
2025-05-28 21:49:24 +0200

    Update definiendo_main.tf

--- docs/definiendo_main.tf
+++ docs/definiendo_main.tf
@@ -1,4 +1,4 @@
-main.tf
+## Definiendo main.tf
 
 #Creacion rg
 
@@ -25,22 +25,22 @@ resource "azurerm_resource_group" "backend" {        #Mismo procedimiento que el
 #Alta usuarios
 
 resource "azuread_user" "users" {                                  #Indicamos con "azuread_user" que el objeto que vamos a crear es un usuario (en este caso un listado)
-  for_each = { for user in local.users : user.email => user }      #Iteramos sobre un listado (se indica que esta en local y se llama users "local.users" y crea un objeto por email en la lista
+  for_each = { for user in local.users : user.email => user }      #Iteramos sobre un listado (se indica que esta en local y se llama users "local.users" y crea un objeto por cada email de la lista
 
   user_principal_name = each.value.email                           #Definimos el valor que va a tener "each.value(cada valor).email" del archivo json de locals.tf
-  display_name        = each.value.name                            #Definimos el valor que va a tener "each.value(cada valor).nombre" del listado
-  department          = each.value.departamento                    #Definimos el valor que va a tener "each.value(cada valor).departamento" dentro del listado
-  job_title           = each.value.puesto                          #Definimos el valor que va a tener "each.value(cada valor).puesto" dentro del listado
-  password            = var.password[each.value.email]
-  force_password_change = true
+  display_name        = each.value.name                            #Definimos el valor que va a tener "each.value(cada valor).nombre" del archivo json de locals.tf
+  department          = each.value.departamento                    #Definimos el valor que va a tener "each.value(cada valor).departamento" del archivo json de locals.tf
+  job_title           = each.value.puesto                          #Definimos el valor que va a tener "each.value(cada valor).puesto" del archivo json de locals.tf
+  password            = var.password[each.value.email]             #Le pasamos la variable password, que definimos en variables.tf, que a su vez se alimenta de secrets.tfvars
+  force_password_change = true                                     #Forzamos que cuando un usuario inicie sesión por primera vez, tenga que cambiar su password
 }
 
 #Asignacion roles
 
-resource "azurerm_role_assignment" "usuarios" {
-  for_each = { for usuario in local.users : usuario.email => usuario }
+resource "azurerm_role_assignment" "usuarios" {                                #Indicamos con "azurerm_role_assignment" que el objeto que vamos a crear es una asignacion de rol
+  for_each = { for usuario in local.users : usuario.email => usuario }         #Iteramos sobre un listado (se indica que esta en local y se llama users "local.users" y crea un objeto por cada email de la lista
 
-  principal_id         = azuread_user.usuarios[each.value.email].object_id
-  scope                = each.value.departamento == "frontend" ? azurerm_resource_group.frontend.id : azurerm_resource_group.backend.id
-  role_definition_name = each.value.puesto == "n1" ? "Reader" : "Contributor"
+  principal_id         = azuread_user.usuarios[each.value.email].object_id     #Define que principal_id será igual que el object_id que Azure crea al dar de alta el usuario usando como clave su email
+  scope                = each.value.departamento == "frontend" ? azurerm_resource_group.frontend.id : azurerm_resource_group.backend.id  #Define SI el departamento (asignado en el json) = frontend, lo dará de alta en el resource group de frontend. Si no cumple esto lo dará de alta en backend
+  role_definition_name = each.value.puesto == "n1" ? "Reader" : "Contributor"  #Define SI el puesto (asignado en el json) = n1, le asignará el rol de Reader. Si no cumple esto, le asignará el rol Contributor (para el puesto n2)
 }
