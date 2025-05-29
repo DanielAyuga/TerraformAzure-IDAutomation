e148c596101008336c649bce1b0f2011abafbfaa
Daniel Ayuga <danielayugachacon@outlook.com>
2025-05-29 09:38:07 +0200

    Update locals.tf

--- deploy/locals.tf
+++ deploy/locals.tf
@@ -1,6 +1,5 @@
 locals {
-  users = {
-    for user in jsondecode(file("${path.module}/users.json")) :
-    user.email => merge(user, { name = user.nombre })
+  users = { for user in jsondecode(file("${path.module}/users.json")) :
+    user.email => user
   }
 }
